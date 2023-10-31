d73e3aaeeca42cb636ecd9d483b4cb00c5b63501
HoracioSadaniswski <rhs9208@gmail.com>
2023-10-31 15:03:38 -0300

    15 - Se agregó un endpoint en ventasController para poder eliminar un registro de ventas junto con la solicitud http correspondiente.

--- controllers/ventasController.js
+++ controllers/ventasController.js
@@ -3,19 +3,44 @@ import Venta from '../models/ventas.js';
 import Carrito from '../models/carrito.js';
 import Producto from '../models/producto.js';
 
+
+export async function listSalesByUserId(req, res) {
+	if (req.nivelUsuario == 2) {
+		return res.status(401).json({'message':'Debes ser Administrador para realizar esta acción'});
+	} else {
+		try {
+			const { usuario_id } = req.params;
+
+			// Buscar las ventas asociadas al usuario específico
+			const ventas = await Venta.findAll({
+				where: { cliente_id: usuario_id },
+			});
+
+			if (ventas.length === 0) {
+				return res.status(404).json({ message: 'No se encontraron ventas para este usuario' });
+			}
+
+			res.status(200).json({ ventas });
+		} catch (error) {
+			console.error('Error al listar ventas:', error);
+			res.status(500).json({ message: 'Error al listar ventas' });
+		}
+	};
+};
+
 export async function createVenta(req, res) {
 	if (req.nivelUsuario !== 1) {
-		return res.status(401).json({'message':'Debes ser Administrador para realizar esta acción'});
+		return res.status(401).json({'message':'Debes ser un usuario autorizado para realizar esta acción'});
 	} else {
 		try {
 			const usuario_id = req.params.usuario_id;
 
-			// Busca el carrito del usuario
+			// Busca el carrito del usuario con detalles de los productos
 			const carrito = await Carrito.findAll({
 				where: {
 					cliente_id: usuario_id,
 				},
-				include: Producto,
+				include: Producto, // Incluye el modelo Producto
 			});
 
 			if (carrito.length === 0) {
@@ -28,16 +53,23 @@ export async function createVenta(req, res) {
 				// Calcula el monto total
 				let montoTotal = 0;
 
-				// Arreglo para registrar los productos vendidos
+				// Arreglo para registrar los productos vendidos en formato JSON
 				const productosVendidos = [];
 
 				for (const item of carrito) {
-					montoTotal += item.cantidad * item.Producto.precio;
+					const producto = item.Producto; // Producto asociado al item
 
-					// Registra los productos vendidos en el arreglo
+					montoTotal += item.cantidad * producto.precio;
+
+					// Registra los productos vendidos en el arreglo en formato JSON
 					productosVendidos.push({
 						producto_id: item.producto_id,
+						nombre_producto: producto.nombre,
 						cantidad: item.cantidad,
+						detalles: {
+							precio: producto.precio,
+							categoria_id: producto.categoria_id,
+						},
 					});
 				}
 
@@ -46,7 +78,7 @@ export async function createVenta(req, res) {
 					cliente_id: usuario_id,
 					fecha_venta: new Date(),
 					monto_total: montoTotal,
-					productos: productosVendidos, // Almacena los productos vendidos en la columna 'productos'
+					productos: productosVendidos,
 				}, { transaction: t });
 
 				// Limpia el carrito
@@ -68,6 +100,36 @@ export async function createVenta(req, res) {
 		} catch (error) {
 			console.error('Error al crear la venta:', error);
 			return res.status(500).json({ message: 'Error al crear la venta' });
+		}
+	}
+};
+
+export async function deleteVentaById(req, res) {
+	if (req.nivelUsuario !== 3) {
+		return res.status(401).json({'message':'Solo un usuario nivel 3 puede borrar un registro de venta'});
+	} else {
+		try {
+			const { usuario_id, venta_id } = req.params;
+
+			// Verificar si la venta existe y pertenece al usuario
+			const venta = await Venta.findOne({
+				where: {
+					id: venta_id,
+					cliente_id: usuario_id,
+				},
+			});
+
+			if (!venta) {
+				return res.status(404).json({ message: 'Venta no encontrada' });
+			}
+
+			// Eliminar la venta
+			await venta.destroy();
+
+			return res.json({ message: 'Venta eliminada exitosamente' });
+		} catch (error) {
+			console.error('Error al eliminar la venta:', error);
+			return res.status(500).json({ message: 'Error al eliminar la venta' });
 		}
 	};
 };
--- routes/ventasRouter.js
+++ routes/ventasRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createVenta } from '../controllers/ventasController.js';
 import tokenAuthentication from '../middlewares/tokenAuthentication.js';
 
 const ventaRouter = express.Router();
 
+import { listSalesByUserId, createVenta, deleteVentaById } from '../controllers/ventasController.js';
 
+ventaRouter.get('/:usuario_id', tokenAuthentication, listSalesByUserId);
 ventaRouter.post('/:usuario_id', tokenAuthentication, createVenta);
+ventaRouter.delete('/:usuario_id/:venta_id', tokenAuthentication, deleteVentaById);
 
 export default ventaRouter;
